6522d9e25f3e7e95025b782fdf14258ed9d7b934
arahouse <arahousemg@gmail.com>
2018-11-22 20:27:05 +0700

    90%
    modals?

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,41 @@
-$(function() {
+$(document).ready(function () {
+    var Events = {},
+        App = {};
+    Events = {
+        formSend: function () {
+            $('form').on('submit', function (e) {
+                e.preventDefault();
+                var form = $(this);
+                form.fadeOut();
+                form.siblings('.callback_loading').fadeIn();
 
-	// Custom JS
+                $.ajax({
+                    url: '/send_message.php',
+                    type: 'post',
+                    data: form.serialize(),
+                    success: function (data) {
+                        form.siblings('.callback_loading').fadeOut();
+                        form.siblings('.success-message').fadeIn();
+                        console.log(data);
+                    },
+                    error: function () {
+                        console.log(data);
+                        form.siblings('.callback_loading').fadeOut();
+                        form.siblings('.error-message').fadeIn();
+                    }
+                });
+                return false;
+            });
+        }
+    };
+
+    App = {
+        init: function () {
+            Events.formSend();
+        }
+    };
+
+
+    App.init();
 
 });
