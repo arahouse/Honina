0f02092c4574f084f13b706c7f757036db39d4fc
arahouse <arahousemg@gmail.com>
2018-11-23 14:56:20 +0700

    first commit

--- app/js/common.js
+++ app/js/common.js
@@ -6,26 +6,34 @@ $(document).ready(function () {
             $('form').on('submit', function (e) {
                 e.preventDefault();
                 var form = $(this);
-                form.fadeOut();
-                form.siblings('.callback_loading').fadeIn();
+                form.find('.row').fadeOut();
+                form.find('.callback_loading').fadeIn();
 
                 $.ajax({
                     url: '/send_message.php',
                     type: 'post',
                     data: form.serialize(),
                     success: function (data) {
-                        form.siblings('.callback_loading').fadeOut();
-                        form.siblings('.success-message').fadeIn();
+                        form.find('.callback_loading').fadeOut();
+                        form.find('.success-message').fadeIn();
                         console.log(data);
                     },
                     error: function () {
                         console.log(data);
-                        form.siblings('.callback_loading').fadeOut();
-                        form.siblings('.error-message').fadeIn();
+                        form.find('.callback_loading').fadeOut();
+                        form.find('.error-message').fadeIn();
                     }
                 });
                 return false;
             });
+            var width = $('.g-recaptcha').parent().width();
+            if (width < 302) {
+                var scale = width / 302;
+                $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('transform-origin', '0 0');
+                $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+            }
         }
     };
 
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Always at the end
+		'app/js/bootstrap.js'
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
